d0f49ac9192627f3212c0125d1da727bf8aee8c6
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-06-15 16:34:36 +0000

    More information added to landing page. Added GET and POST routes "/applications". Added ability for user to add to the list of applications.
    Basic styling completed via Bootstrap 3.3.7. Next focus: style /applications page as well as Nav and Forms.

--- edtechyelp/app.js
+++ edtechyelp/app.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+var applications = [
+		{name: "App # 1", image: "https://source.unsplash.com/hes6nUC1MVc"},
+		{name: "App # 2", image: "https://source.unsplash.com/hes6nUC1MVc"},
+		{name: "App # 3", image: "https://source.unsplash.com/hes6nUC1MVc"}
+	]
+	
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -8,18 +17,23 @@ app.get("/", function(req, res){
 });
 
 app.get("/applications", function(req, res){
-	
-	//need to replace template applications with data from eduappcenter.com API
-	//https://www.eduappcenter.com/api/v1/lti_apps
-	var applications = [
-		{name: "App # 1", image: "https://source.unsplash.com/hes6nUC1MVc"},
-		{name: "App # 2", image: "https://source.unsplash.com/hes6nUC1MVc"},
-		{name: "App # 3", image: "https://source.unsplash.com/hes6nUC1MVc"}
-	]
-	
 	res.render("applications", {applications: applications});
-} );
+});
+
+app.post("/applications", function(req, res){
+    //get data from form and add to applications array.
+    var name = req.body.name;
+    var image = req.body.image;
+    var newApplication = {name: name, image: image};
+    applications.push(newApplication);
+    //redirect back to applications page
+    res.redirect("/applications");
+});
+
+app.get("/applications/new", function(req, res) {
+    res.render("new.ejs");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The server has started!");
-})
+});
