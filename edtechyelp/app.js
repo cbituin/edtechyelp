c4bb20fdf129062de13e4dc9171a8c8730d6672b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-06-26 16:36:23 +0000

    Added ability for user to make comment on all edtech applications. //NEXT TODO - Styling for all pages

--- edtechyelp/app.js
+++ edtechyelp/app.js
@@ -1,9 +1,10 @@
-var express 	= require("express"),
-	app 		= express(),
-	bodyParser	= require("body-parser"),
-	mongoose	= require("mongoose"),
-	Application = require("./models/application"),
-	seedDB		= require("./seeds");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Application = require("./models/application"),
+    seedDB      = require("./seeds"),
+    Comment		= require("./models/comment");
 
 mongoose.connect("mongodb://localhost/edtechyelp");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,14 +17,14 @@ app.get("/", function(req, res){
 });
 
 app.get("/applications", function(req, res){
-	//Get all applications from DB
-	Application.find({}, function(err, allApplications){
-		if(err){
-			console.log(err);
-		} else {
-			res.render("applications/index", {applications:allApplications});
-		}
-	});
+    //Get all applications from DB
+    Application.find({}, function(err, allApplications){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("applications/index", {applications:allApplications});
+        }
+    });
 });
 
 app.post("/applications", function(req, res){
@@ -35,11 +36,11 @@ app.post("/applications", function(req, res){
     var newApplication = {name: name, image: image, summary: summary, url: url};
     // Create a new application and save to DB 
        Application.create(newApplication, function(err, newlyCreated){
-    	if(err){
-    		console.log(err);
-    	} else {
-    		res.redirect("applications");
-    	}
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("applications");
+        }
     });
 });
 
@@ -48,15 +49,15 @@ app.get("/applications/new", function(req, res) {
 });
 
 app.get("/applications/:id", function(req, res){
-	//find the application with the provided id
-	Application.findById(req.params.id).populate("comments").exec(function(err, foundApplication){
-		if(err){
-			console.log(err);
-		}	else {
-			console.log(foundApplication);
-			res.render("applications/show", {application: foundApplication});
-		}
-	});
+    //find the application with the provided id
+    Application.findById(req.params.id).populate("comments").exec(function(err, foundApplication){
+        if(err){
+            console.log(err);
+        }   else {
+            console.log(foundApplication);
+            res.render("applications/show", {application: foundApplication});
+        }
+    });
 });
 
 //===================
@@ -64,21 +65,33 @@ app.get("/applications/:id", function(req, res){
 //===================
 
 app.get("/applications/:id/comments/new", function(req, res) {
-	Application.findById(req.params.id, function(err, application){
-		if(err){
-			console.log(err);
-		} else {
-			res.render("comments/new", {application: application});
-		}
-	})
+    Application.findById(req.params.id, function(err, application){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {application: application});
+        }
+    });
 });
 
 app.post("/applications/:id/comments", function(req, res){
-	//lookup campground using ID
-	//create new comment
-	//connect new comment to campground
-	//redirect campground show page
-})
+    //lookup campground using ID
+    Application.findById(req.params.id, function(err, application) {
+        if(err){
+            console.log(err);
+            res.redirect("/applications");
+        }else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+            } else {
+                application.comments.push(comment);
+                application.save();
+                res.redirect('/applications/' + application._id);
+            } 
+        });
+    }});
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The server has started!");
