1cfea095aa7fd654ee0abf2029f9fb24119b84e8
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-07-09 20:20:56 +0000

    Added ability to edit comments

--- edtechyelp/routes/comments.js
+++ edtechyelp/routes/comments.js
@@ -35,6 +35,31 @@ router.post("/applications/:id/comments", isLoggedIn, function(req, res){
     }});
 });
 
+//COMMENTS EDIT
+router.get("/applications/:id/comments/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {application_id: req.params.id, comment: foundComment});
+
+        }
+    });
+});
+
+//COMMENTS UPDATE ROUTE
+router.put("/applications/:id/comments/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComments){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/applications/" + req.params.id);
+        }
+    });
+});
+
+
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
